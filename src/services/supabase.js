ccff3f5d863ba6d85d9d4a94dbdcbfe50f225cd8
ujjwal <ujjwalbaranwal221715@gmail.com>
2024-06-16 00:50:15 +0530

    supabase is setted up

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://aejtjzfrmrkixowcefxg.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlanRqemZybXJraXhvd2NlZnhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0NzQ4NjUsImV4cCI6MjAzNDA1MDg2NX0.pUbS768oOJ4_Lg_FkTTKwNdDDuhTFkUye-vAIU1_e5U";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
